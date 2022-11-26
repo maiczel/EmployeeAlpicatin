05b7e6975fbcdc2522bc141dd821d44cbc63df5b
Thomas <t.machaj>
2022-11-26 17:42:45 +0100

    Test commit

---
+++ src/main/java/com/employee/EmployeeAlpicatin/EmployeeAlpicatinApplication.java
@@ -0,0 +1,13 @@
+package com.employee.EmployeeAlpicatin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeAlpicatinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeAlpicatinApplication.class, args);
+	}
+
+}
